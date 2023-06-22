6a5ecad86517d5f552b6f3fba742bf0b4beef197
Dolores Polito <dolores.polito@gmail.com>
2023-06-22 10:58:04 -0300

    pricipal route

--- cavaoseca/src/App.js
+++ cavaoseca/src/App.js
@@ -12,7 +12,7 @@ function App() {
  
       <Routes>
         <>
-          <Route path="/" element={<Home />} />
+          <Route path="/cavaoseca" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
